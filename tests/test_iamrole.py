8e78f649894d332d238047a4214499535b508c8f
esantix <santiago93echevarria@gmail.com>
2024-08-15 01:33:53 -0300

    fix test import

--- tests/test_iamrole.py
+++ tests/test_iamrole.py
@@ -1,9 +1,9 @@
 import unittest
-from awspolicies.principal import IAMRole
+from awspolicies.principal import IamRole
 
 
 class TestIAMRole(unittest.TestCase):
 
     def test_placeholder(self):
-        IAMRole(Name="Fake_role", Arn="FAkeARN")
+        IamRole(Name="Fake_role", Arn="FAkeARN")
         self.assertTrue(True)
